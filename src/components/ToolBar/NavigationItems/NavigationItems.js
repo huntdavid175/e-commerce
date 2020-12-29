5be32283d6aeb5e7f2abd6600a3a232b331ce3b2
huntdavid175 <huntdavid175@gmail.com>
2020-12-29 17:02:06 +0000

    added react icons to project

--- src/components/ToolBar/NavigationItems/NavigationItems.js
+++ src/components/ToolBar/NavigationItems/NavigationItems.js
@@ -2,15 +2,19 @@ import React from "react";
 import NavigationItem from "../NavigationItem/NavigationItem";
 import classes from "./NavigationItems.module.css";
 import Spring from "../../Layout/Spring";
+import { FiShoppingCart } from "react-icons/fi";
 
 const NavigationItems = (props) => {
   return (
     <Spring>
-    <ul className={classes.NavigationItems}>
-      <NavigationItem>Sign up</NavigationItem>
-      <NavigationItem>Sign in</NavigationItem>
-      <NavigationItem>Cart (0)</NavigationItem>
-    </ul>
+      <ul className={classes.NavigationItems}>
+        <NavigationItem>Sign up</NavigationItem>
+        <NavigationItem>Sign in</NavigationItem>
+        <NavigationItem>
+          <FiShoppingCart style={{ marginRight: "3px" }} />
+          Cart (0)
+        </NavigationItem>
+      </ul>
     </Spring>
   );
 };
